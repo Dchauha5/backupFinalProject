4d6ea734b582a214e9f19e9a2091ae4b0f0fb73e
Dhruvenkumar Chauhan <dhruven@DhruvenumarsMBP.attlocal.net>
2020-12-18 02:06:23 -0500

    backup

---
+++ personal-budget/src/app/homepage/homepage.component.ts
@@ -0,0 +1,89 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Chart } from 'chart.js';
+
+@Component({
+  selector: 'pb-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.scss']
+})
+export class HomepageComponent implements OnInit {
+
+  title:any;
+  amount:number;
+
+public dataSource =  {
+  datasets: [
+  {
+          data: [],
+          backgroundColor: [
+          '#ffcd56',
+          '#ff6384',
+          '#36a2eb',
+          '#fd6b19',
+      'black',
+      'green',
+      'red'
+          ],
+}  ],
+
+  labels: []
+};
+
+  constructor(private http: HttpClient) { }
+  ngOnInit(): void {
+    this.http.get('http://localhost:3000/budget')
+    .subscribe((res: any) => {
+      for(var i = 0 ; i < res.length; i++)
+      {
+
+          this.dataSource.datasets[0].data[i] = res[i].budget;
+          this.dataSource.labels[i] = res[i].title;
+          this.createChart();
+  }
+
+    });
+
+}
+
+createChart() {
+  var ctx = document.getElementById('myChartPie');
+  var myPieChart = new Chart(ctx , {
+      type: 'pie',
+      data: this.dataSource
+    });
+
+  var ctx = document.getElementById('myChartBar');  
+  var mixedChart = new Chart(ctx, {
+    type: 'bar',
+    data: this.dataSource,
+     });
+
+
+  var ctx = document.getElementById('polarArea'); 
+  var mixedChart = new Chart(ctx, {
+    type: 'polarArea',
+    data: this.dataSource,
+     });
+
+  var ctx = document.getElementById('line'); 
+  var mixedChart = new Chart(ctx, {
+    type: 'line',
+    data: this.dataSource,
+     });
+     
+  }
+
+updateBudget(){
+  let body = {
+    title : this.title,
+    budget : this.amount
+  }
+  this.http.post("http://localhost:3000/addBudget",body)
+    .subscribe((res: any) => {
+    window.alert("Data Updated")
+    })
+    this.createChart()
+    this.ngOnInit()
+  }
+}
---
+++ server/server.js
@@ -0,0 +1,67 @@
+//Budget API
+const express = require('express');
+const cors = require("cors");
+const mongoose = require("mongoose");
+const chartDataSchema = require("./models/chart_data_schema");
+
+const bodyParser = require('body-parser');
+
+const port = 3000;
+let url = 'mongodb://localhost:27017/personalBudget';
+
+const app = express();
+app.use('/', express.static('public'));
+app.use(express.json());
+app.use(cors());
+
+
+
+app.get("/hello", (req, res) => {
+    console.log("Hello");
+    res.json({
+        data: "Hello"
+    });
+});
+
+app.get("/budget", (req, res) => {
+    mongoose.connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        .then(() => {
+            chartDataSchema.find({}).then(data => {
+                res.json(data);
+                mongoose.connection.close();
+            }).catch(err => {
+                console.log(err);
+            });
+        }).catch(err => {
+            console.log(err);
+        });
+});
+
+
+app.post("/addBudget", (req, res) => {
+    console.log("ADDDDDDDDDDD");
+    console.log(req.body);
+    mongoose.connect(url).then(() => {
+        var chartData = {
+            title: req.body.title,
+            budget: req.body.budget,
+        };
+
+        chartDataSchema.insertMany(chartData).then((data) => {
+            res.json(data);
+
+            mongoose.connection.close();
+        }).catch(connectionError => {
+            console.log(connectionError);
+        });
+    }).catch(connectionError => {
+        console.log(connectionError);
+    });
+})
+
+app.listen(port, () => {
+    console.log(`API served  at http://localhost:${port}`);
+});
